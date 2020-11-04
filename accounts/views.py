cdd9a73d0a1828c7a5c7dc15cfb73b48467d22d3
Cyril Gouverneur <cyril@imac-de-cyril.home>
2020-11-04 11:09:21 +0100

    Create User Login

--- accounts/views.py
+++ accounts/views.py
@@ -52,7 +52,19 @@ def login(request) :
 
     # Login User
     if request.method == 'POST' :
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None :
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        
+        else :
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     
     else :
         return render(request, 'accounts/login.html')
